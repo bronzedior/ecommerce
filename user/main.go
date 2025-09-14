c34f7e1a7b48d7a8a5b2704a5914bfb00b98e865
Zedior Donny <zediordonny@gmail.com>
2025-09-15 00:50:28 +0700

    feat(user): create db and redis connection

--- user/main.go
+++ user/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"user/cmd/user/handler"
 	"user/config"
 	"user/infrastructure/log"
+	"user/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,8 +14,11 @@ func main() {
 
 	log.SetupLogger()
 
+	userHandler := handler.NewUserHandler()
+
 	port := cfg.App.Port
 	router := gin.Default()
+	routes.SetupRoutes(router, *userHandler)
 	router.Run(":" + port)
 
 	log.Logger.Printf("Server running on port: %s", port)
