9df4db84c873604e8e292949ba98614420a96864
Zedior Donny <zediordonny@gmail.com>
2025-09-15 14:39:24 +0700

    feat(product): create middleware request log

--- product/routes/routes.go
+++ product/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"product/cmd/product/handler"
+	"product/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine, productHandler handler.ProductHandler) {
+	router.Use(middleware.RequestLogger())
 	router.GET("/ping", productHandler.Ping)
 }
