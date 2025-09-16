17932f222f0cf1a52eb4d61ded72007e34f950dc
Zedior Donny <zediordonny@gmail.com>
2025-09-16 20:33:55 +0700

    feat(order): create middleware request log

--- order/routes/routes.go
+++ order/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"order/cmd/order/handler"
+	"order/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine, orderHandler handler.OrderHandler) {
+	router.Use(middleware.RequestLogger())
 	router.GET("/ping", orderHandler.Ping)
 }
