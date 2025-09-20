4918b9ea46b56514f724195bb656d2689cdff226
Zedior Donny <zediordonny@gmail.com>
2025-09-20 21:03:50 +0700

    feat(payment): create middleware request log

--- payment/routes/routes.go
+++ payment/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"payment/cmd/payment/handler"
+	"payment/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine, paymentHandler handler.PaymentHandler) {
+	router.Use(middleware.RequestLogger())
 	router.GET("/ping", paymentHandler.Ping)
 }
